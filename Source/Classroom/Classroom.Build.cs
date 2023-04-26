ae9213c8ab995bf90ff238336646dcbbac61431b
Oonoo19 <lizzbetgrant1009@gmail.com>
2023-04-26 18:21:20 +0800

    file drag&drop

--- Source/Classroom/Classroom.Build.cs
+++ Source/Classroom/Classroom.Build.cs
@@ -8,7 +8,7 @@ public class Classroom : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "PlayFabGSDK", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG" });
         PrivateDependencyModuleNames.AddRange(new string[] { "PlayFab", "PlayFabCpp", "PlayFabCommon" });
 
         MinFilesUsingPrecompiledHeaderOverride = 1;
